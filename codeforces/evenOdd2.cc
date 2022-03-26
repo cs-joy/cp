2a5aee73f7278edf17c1f5a26a3e14db2174a0e6
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-03-26 08:06:33 +0600

    init up

--- codeforces/evenOdd2.cc
+++ codeforces/evenOdd2.cc
@@ -11,13 +11,23 @@ int main() {
         char n[100];
         cin >> n;
         int a = strlen(n)-1;
-        int b=n[a]-48;
-        if(b%2 == 0) {
+
+        if(strlen(n) <= 100) {
+            int b=n[a]-48;
+            
+            if(b%2 == 0) {
             cout << "even" << endl;
+            cout << strlen(n);
         }
         else {
             cout << "odd\n";
+            cout << strlen(n);
         }
+        }
+        else {
+            cout << "Overload Character!! Maximum Limit is 100 but you are inputing - " << strlen(n) << "character" << endl;
+        }
+        
     }
 
     return 0;
