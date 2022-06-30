b173bd5ed1fa542cfde14860a8c68f45f4adbb1c
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-06-30 20:41:38 +0600

    Update palindrome.cpp

--- googleKickstart/palindrome.cpp
+++ googleKickstart/palindrome.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
+#include <string>
+#include <cstring>
 
 using namespace std;
 
-int main() {
+void palindromeNumber(){
     int n, num, digit, rev=0;
 
     cout << "Enter a number";
@@ -24,7 +26,55 @@ int main() {
     } else {
          cout << " The number is not a palindrome.";
     }
+}
+/*
+void palindromString(){
+    string str, check, reverseCharacter=0, character;
+
+    cout << "enter a string: ";
+    cin >> str;
+
+    check = str;
+    do{
+        character = str % 10;
+        reverseCharacter = (reverseCharacter * 10) + character;
+        str = str / 10;
+    } while(str != 0);
+
+    cout << "rev: " << reverseCharacter;
+
+}
+*/
+
+void pS(){
+    char string[20];
+    int length;
+    int flag = 0;
     
+    printf("Enter a string:");
+    scanf("%s", string);
+    
+    length = strlen(string);
+    
+    for(int i=0;i < length ;i++){
+        if(string[i] != string[length-i-1]){
+            flag = 1;
+            break;
+           }
+        }
+    
+    if (flag) {
+        printf("%s is not a palindrome", string);
+    }    
+    else {
+        printf("%s is a palindrome", string);
+    }
+}
+
+int main() {
+    
+    //palindromeNumber();
+    pS();
 
     return 0;
 }
